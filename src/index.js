11e8aedd3994071476b0e95ec36d40f645d68515
Lucas <l.duarte.mk@gmail.com>
2019-08-02 16:44:21 -0300

    removing files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './app'
+import * as serviceWorker from './serviceWorker'
 import ErrorBoundary from './error'
 
 ReactDOM.render(
@@ -12,9 +12,6 @@ ReactDOM.render(
     )}
     </ErrorBoundary>, 
     document.getElementById('root')
-);
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
