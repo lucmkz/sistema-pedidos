c2341ee7dac0d9562a109b1b9aff85456ee0b19f
Lucas <l.duarte.mk@gmail.com>
2019-08-05 23:55:55 -0300

    style login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,24 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Button, Grid } from '@material-ui/core'
-import { ReactComponent as Logo } from './logo-react-zzaria.svg'
+import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
 
 const Login = () => (
   <Container>
-    <Grid container direction='column' alignItems='center' spacing={10}>
-      <Grid iten>
+    <Grid container justify='center' spacing={7}>
+      <Grid item>
         <Logo />
       </Grid>
 
-      <Grid iten>
-        <Button>Entrar com GitHub</Button>
+      <Grid item xs={12}>
+        <GitHubButton variant='contained' fullWidth>
+          Entrar com GitHub
+        </GitHubButton>
       </Grid>
     </Grid>
   </Container>
 )
 
 const Container = styled.div`
-  padding: 80px
+  padding: 20px;
+`
+
+const Logo = styled(MainLogo)`
+  width: 100%;
+`
+
+const GitHubButton = styled(Button)`
+  && {
+    font-size: 20px;
+    padding: 15px;
+    text-transform: none;
+  }
 `
 
 export default Login
