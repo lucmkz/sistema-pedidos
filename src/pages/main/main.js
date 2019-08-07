d6bd269aeab82d8e7e18880a3fdda34ba6f25f56
Lucas <l.duarte.mk@gmail.com>
2019-08-07 17:00:14 -0300

    continuação edição HEADER do Main

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,7 +2,10 @@ import React from 'react'
 import {
   AppBar,
   Toolbar,
-  IconButton
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem
 } from '@material-ui/core'
 import { AccountCircle } from '@material-ui/icons'
 import { ReactComponent as MainLogo } from '../login/logo-react-zzaria.svg'
@@ -11,9 +14,20 @@ const Main = () => (
   <AppBar>
     <Toolbar>
       <MainLogo />
+
+      <Typography color='inherit'>
+        Olá joão
+      </Typography>
+
       <IconButton color='inherit'>
         <AccountCircle />
       </IconButton>
+
+      <Menu open>
+        <MenuItem>
+          Sair
+        </MenuItem>
+      </Menu>
     </Toolbar>
   </AppBar>
 )
