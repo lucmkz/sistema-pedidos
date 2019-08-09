b1c24e8250649e2cccdd97524cd08deca5939c66
Lucas <l.duarte.mk@gmail.com>
2019-08-09 11:29:11 -0300

    Updates

---
+++ precache-manifest.10a790025500ef31f966c77ca01d063d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ab3713fefa0864db7bf85cc8d9dfca",
+    "url": "/sistema-pedidos/index.html"
+  },
+  {
+    "revision": "b609a943bb1a5342bb74",
+    "url": "/sistema-pedidos/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "005db834f419e0b8c98b",
+    "url": "/sistema-pedidos/static/js/0.61ca9e90.chunk.js"
+  },
+  {
+    "revision": "a6be7418310054f8192e",
+    "url": "/sistema-pedidos/static/js/3.ce228cff.chunk.js"
+  },
+  {
+    "revision": "b3a1cc0e21943f057038",
+    "url": "/sistema-pedidos/static/js/4.63e29e0e.chunk.js"
+  },
+  {
+    "revision": "da5ee2e30109e9661149",
+    "url": "/sistema-pedidos/static/js/5.b1168338.chunk.js"
+  },
+  {
+    "revision": "8cc9873ab8f9d3ad291d",
+    "url": "/sistema-pedidos/static/js/6.de72181c.chunk.js"
+  },
+  {
+    "revision": "96381a8049a141d1304f",
+    "url": "/sistema-pedidos/static/js/7.11a6872a.chunk.js"
+  },
+  {
+    "revision": "b609a943bb1a5342bb74",
+    "url": "/sistema-pedidos/static/js/main.b549c0ba.chunk.js"
+  },
+  {
+    "revision": "23526ae1176f4476cd7b",
+    "url": "/sistema-pedidos/static/js/runtime~main.2d0d1dc5.js"
+  },
+  {
+    "revision": "462b499972a44670a011d97cb8e5d5f6",
+    "url": "/sistema-pedidos/static/media/logo-react-zzaria.462b4999.svg"
+  }
+]);
