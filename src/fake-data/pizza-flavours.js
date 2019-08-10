73f0d300087128dcce349421add0499ce8f2193b
Lucas <l.duarte.mk@gmail.com>
2019-08-10 09:44:52 -0300

    Create data structure for pizza flavours page

---
+++ src/fake-data/pizza-flavours.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 0,
+    image: '/fake-data/images/pizza-frango.jpeg',
+    name: 'Frango',
+    values: {
+      '0': 20,
+      '1': 30,
+      '2': 40
+    }
+  },
+  {
+    id: 1,
+    image: '/fake-data/images/pizza-milho.jpeg',
+    name: 'Milho',
+    values: {
+      '0': 25,
+      '1': 35,
+      '2': 45
+    }
+  },
+  {
+    id: 2,
+    image: '/fake-data/images/pizza-portuguesa.jpg',
+    name: 'Frango',
+    values: {
+      '0': 28,
+      '1': 38,
+      '2': 48
+    }
+  }
+]
