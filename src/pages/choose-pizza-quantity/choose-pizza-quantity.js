f94b2062de27f3b591f3af31ffd6f7cc7c5f396a
Lucas <l.duarte.mk@gmail.com>
2019-08-14 16:18:36 -0300

    add rota quantity e melhorando import

---
+++ src/pages/choose-pizza-quantity/choose-pizza-quantity.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ChoosePizzaQuantity () {
+  return (<h1>top</h1>)
+}
+
+export default ChoosePizzaQuantity
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,10 +2,11 @@ import React, { Suspense } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { withStyles, LinearProgress } from '@material-ui/core'
 import Header from './header'
-import { HOME, CHOOSE_PIZZA_FLAVOURS } from 'routes'
+import * as routes from 'routes'
 
 const ChoosePizzaSize = React.lazy(() => import('pages/choose-pizza-size'))
 const ChoosePizzaFlavours = React.lazy(() => import('pages/choose-pizza-flavours'))
+const ChoosePizzaQuantity = React.lazy(() => import('pages/choose-pizza-quantity'))
 
 const Main = () => {
   return (
@@ -16,8 +17,9 @@ const Main = () => {
 
       <Suspense fallback={<LinearProgress />}>
         <Switch>
-          <Route path={HOME} exact component={ChoosePizzaSize}/>
-          <Route path={CHOOSE_PIZZA_FLAVOURS} component={ChoosePizzaFlavours}/>
+          <Route path={routes.HOME} exact component={ChoosePizzaSize}/>
+          <Route path={routes.CHOOSE_PIZZA_FLAVOURS} component={ChoosePizzaFlavours}/>
+          <Route path={routes.CHOOSE_PIZZA_QUANTITY} component={ChoosePizzaQuantity}/>
         </Switch>
       </Suspense>
     </>
